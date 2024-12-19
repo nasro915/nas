e476cce01525897c920c1a10cb206242e14c776c
nasro915 <nasriina7337@gmail.com>
2024-12-19 21:22:04 +0300

    first commit

---
+++ Script.js
@@ -0,0 +1,4 @@
+function changeButtonColor() {
+    const button = document.querySelector("button");
+    button.style.backgroundColor = button.style.backgroundColor === "blue" ? "#4CAF50" : "blue";
+}
